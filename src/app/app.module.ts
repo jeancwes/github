2c10540ac570bed3a410a85147ddec42969f0905
jeancwes <jeancwes@gmail.com>
2021-03-21 14:19:58 -0300

    Added user and search modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchModule } from './modules/search/search.module';
+import { UserModule } from './modules/user/user.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    SearchModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
