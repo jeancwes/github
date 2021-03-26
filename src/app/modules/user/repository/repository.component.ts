5be3baa1a3340e74940088d0460ec12455b70f28
jeancwes <jeancwes@gmail.com>
2021-03-26 19:32:14 -0300

    Added user repositories.

--- src/app/modules/user/repository/repository.component.ts
+++ src/app/modules/user/repository/repository.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-repository',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepositoryComponent implements OnInit {
 
-  constructor() { }
+  @Input() userLogin: string;
+
+  public repositories: any = [];
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private httpClient: HttpClient,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(simpleChanges: SimpleChanges) {
+    console.log('SimpleChanges');
+    if (!simpleChanges.userLogin.currentValue) {
+      return;
+    }
+    this.getUserRepositories();
+  }
+
+  getUserRepositories() {
+    this.httpClient.get(`${environment.serverUrl}/users/${this.userLogin}/repos`)
+      .subscribe((response: []) => {
+        console.log(response);
+        if (response) {
+          this.repositories = this.sort(response);
+        }
+      });
+  }
+  
+  sort(repositories: []) {
+    return repositories.sort((a: any, b: any) => { 
+      if(a.stargazers_count < b.stargazers_count) {
+        return 1;
+      }
+      return -1;
+    });
   }
 
 }
--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { RepositoryComponent } from './repository/repository.component';
 import { UserComponent } from './user/user.component';
 
 
 const routes: Routes = [
   { path: 'user/:userLogin', component: UserComponent },
-  { path: 'repository', component: RepositoryComponent,
-    children: [
-    ],
-  },
   { path: '', redirectTo: 'user/:userLogin', pathMatch: 'full' }
 ];
 
